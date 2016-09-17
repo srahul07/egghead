d9a9ea45c99b17e3f2969fd3b95694b9d82441a7
Rahul Shelke <srahul07@gmail.com>
2016-09-17 19:18:03 +0530

    Adding rendering multiple components

--- react/App.js
+++ react/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+
+// With state component creation
 class App extends React.Component {
   render(){
-    return <div>Hello Rahul</div>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    );
+    // return React.createElement('h1', null, 'Hello Guys')
   }
 }
+
+// State less component creation
+//const App = () => <h1>Hello Eggheads</h1>
 
 export default App
