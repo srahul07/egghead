badc2ecbb7ec24b5df9172fade533c8dff9ca6f7
Rahul Shelke <srahul07@gmail.com>
2018-09-20 18:28:06 +0530

    Adding first component

--- react/taniarascia/react-tutorial/src/index.js
+++ react/taniarascia/react-tutorial/src/index.js
@@ -1,16 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-            </div>
-        );
-    }
-}
-
-// export default App;
 ReactDOM.render(<App />, document.getElementById('root'));
