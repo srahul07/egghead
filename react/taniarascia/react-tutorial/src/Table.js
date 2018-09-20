73429fe97135146357e01702a0fa75311e61d877
Rahul Shelke <srahul07@gmail.com>
2018-09-20 18:59:46 +0530

    With simple component

--- react/taniarascia/react-tutorial/src/Table.js
+++ react/taniarascia/react-tutorial/src/Table.js
@@ -1,34 +1,46 @@
 import React, { Component } from 'react';
 
 
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th> Name </th>
+                <th> Job </th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <th>Charlie</th>
+                <th>Janitor</th>
+            </tr>
+            <tr>
+                <th>Mac</th>
+                <th>Bouncer</th>
+            </tr>
+            <tr>
+                <th>Dee</th>
+                <th>Aspiring actress</th>
+            </tr>
+            <tr>
+                <th>Dennis</th>
+                <th>Bartender</th>
+            </tr>
+        </tbody>
+    );
+}
+
 class Table extends Component {
     render() {
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th> Name </th>
-                        <th> Job </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>Charlie</th>
-                        <th>Janitor</th>
-                    </tr>
-                    <tr>
-                        <th>Mac</th>
-                        <th>Bouncer</th>
-                    </tr>
-                    <tr>
-                        <th>Dee</th>
-                        <th>Aspiring actress</th>
-                    </tr>
-                    <tr>
-                        <th>Dennis</th>
-                        <th>Bartender</th>
-                    </tr>
-                </tbody>
+                <TableHeader />
+                <TableBody />
             </table>
         );
     }
