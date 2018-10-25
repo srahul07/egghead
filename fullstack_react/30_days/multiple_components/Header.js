a1f9bc091f096502da201b303848cb494fcd50c3
Rahul Shelke <srahul07@gmail.com>
2018-10-25 08:40:19 +0530

    Adding testing

---
+++ fullstack_react/30_days/multiple_components/Header.js
@@ -0,0 +1,14 @@
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <div className="fa fa-more"></div>
+                <span className="title">Timeline</span>
+                <input type="text" className="searchInput" placeholder="Search ..." />
+                <div className="fa fa-search searchIcon"></div>
+            </div>
+        );
+    }
+}
+
+export default Header;
